015f6d1871867fc2cf782c7eb5a84504ddc8d7ed
Genar Codina Reverter <genar.codina.reverter@everis.com>
2020-10-04 20:48:21 +0200

    ADD: Added search bar

--- MusicNetwork/Modules/MusicItems/View/MusicItemsViewController.swift
+++ MusicNetwork/Modules/MusicItems/View/MusicItemsViewController.swift
@@ -10,17 +10,24 @@ import UIKit
 
 class MusicItemsViewController: UIViewController {
     
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var musicItemsTableView: UITableView!
     
     let kMusicItemCellIdentifier: String = "MusicItemTableViewCell"
     let kDetailMusicSegue = "DetailMusicSegue"
     var musicItems: [MusicItem] = []
     var presenter:MusicItemsPresenterInterface?
+    var selectedIndexPath: IndexPath = IndexPath(row: 0, section: 0)
+    
+    static let kTrackTimeMillis: String = "trackTimeMillis"
+    static let kPrimaryGenreName: String = "primaryGenreName"
+    static let kTrackPrice: String = "trackPrice"
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         presenter?.fetchMusicItems()
+        self.searchBar.delegate = self
     }
     
     // MARK: - Navigation
@@ -32,6 +39,22 @@ class MusicItemsViewController: UIViewController {
                let indexPath = musicItemsTableView.indexPathForSelectedRow {
                     viewController.musicItem = musicItems[indexPath.row]
             }
+        }
+    }
+    
+    private func sortItems() {
+        
+        if (self.searchBar.selectedScopeButtonIndex == 0) {
+            self.musicItems.sort { (first, second) -> Bool in
+                return first.trackTimeMillis < second.trackTimeMillis
+            }
+        } else if (self.searchBar.selectedScopeButtonIndex == 1) {
+            self.musicItems.sort { (first, second) -> Bool in
+                return first.primaryGenreName < second.primaryGenreName
+            }
+        } else if (self.searchBar.selectedScopeButtonIndex == 2) {
+//            NSSortDescriptor *valueDescriptor = [NSSortDescriptor sortDescriptorWithKey:kTrackPrice ascending:YES];
+//            musicItems = [musicItems sortedArrayUsingDescriptors:@[valueDescriptor]];
         }
     }
 }
@@ -74,6 +97,47 @@ extension MusicItemsViewController: MusicItemsViewInterface {
     func showMusicItems(for musicItems: [MusicItem]) {
         
         self.musicItems = musicItems
+        musicItemsTableView.reloadData()
+    }
+}
+
+extension MusicItemsViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchText.count == 0 {
+            self.view.endEditing(true)
+            self.musicItems = []
+            self.musicItemsTableView.reloadData()
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        if var inputText: String = searchBar.text {
+            let isTextEmpty: Bool = inputText.count == 0
+            let isTextAllWhiteSpaces: Bool = inputText.trimmingCharacters(in: CharacterSet.whitespaces).count == 0
+            
+                if ( !isTextEmpty || !isTextAllWhiteSpaces) {
+                    //NSError *error = nil;
+                    //NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"  +" options:NSRegularExpressionCaseInsensitive error:&error];
+            
+                    //NSString* trimmedString = [regex stringByReplacingMatchesInString:inputText options:0 range:NSMakeRange(0, [inputText length]) withTemplate:@" "];
+                    //NSString* stringWithPlus = [trimmedString stringByReplacingOccurrencesOfString:@" " withString:@"+"];
+                    self.view.endEditing(true)
+                    //ConnectionManager.sharedInstance.searchWithString:stringWithPlus withLimit:kNumberOfSongs
+                }
+        }
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        
+        self.view.endEditing(true)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        
+        self.sortItems()
         musicItemsTableView.reloadData()
     }
 }
